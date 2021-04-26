afbdf7e5514ff028401002b9f1552dd76a61c776
LocketGoma <nothappa@naver.com>
2021-04-26 20:55:20 +0900

    할것 : 휠 적용. 카메라 위치 최적화

---
+++ Project_ShootingStarV2/Assets/Scripts/Player/PlayerShooterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooterController : MonoBehaviour
+{
+    enum eToolState
+    {
+        Idle,Handgun,Rifle,Throwable
+    };
+    [SerializeField] private eToolState toolState;
+    [SerializeField] private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fScroll = Input.GetAxisRaw("Mouse ScrollWheel");
+    }
+}
